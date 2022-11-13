a13bc5f96e151253a00a09e05400e2d7cecc1e74
yasincey <109511189+yasincey@users.noreply.github.com>
2022-11-13 12:23:45 +0300

    c piscine projects

---
+++ C_Piscine/c02/ex10/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yceylan < yceylan@student.42istanbul.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/04 18:36:10 by yceylan           #+#    #+#             */
+/*   Updated: 2022/04/04 18:37:54 by yceylan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	b;
+
+	i = 0;
+	b = 0;
+	while (src[i] != '\0')
+	{	
+		i++;
+		b++;
+	}
+	i = 0;
+	while (src[i] != '\0' && i < size -1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (b);
+}
---
+++ C_Piscine/c06/ex03/ft_sort_params.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yceylan < yceylan@student.42istanbul.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 18:11:46 by yceylan           #+#    #+#             */
+/*   Updated: 2022/04/13 18:11:49 by yceylan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int	main(int argc, char **argv)
+{
+	int	j;
+
+	j = 1;
+	while (j < argc - 1)
+	{
+		if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+		{
+			ft_swap(&argv[j], &argv[j + 1]);
+			j = 1;
+		}
+		else
+			j++;
+	}
+	j = 1;
+	while (j < argc)
+	{
+		ft_putstr(argv[j]);
+		write(1, "\n", 1);
+		j++;
+	}
+	return (0);
+}
